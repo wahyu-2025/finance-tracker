18eeecbb76bcccdee56c347771fcabdaaf53ef80
wahyu-2025 <wahyuprihantoro081004@gmail.com>
2026-08-21 13:49:56 +0700

    update swagger url

--- backend/src/helpers/express/swagger.ts
+++ backend/src/helpers/express/swagger.ts
@@ -9,6 +9,14 @@ export class SwaggerHelper {
             explorer: false,
         };
 
+        // Dynamically override the server URL at runtime
+        swaggerDocument.servers = [
+            {
+                url: config.get<string>('server.host_swagger'),
+                description: 'Environment ' + config.get<string>('app.env')
+            }
+        ];
+
         app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
     }
 }
